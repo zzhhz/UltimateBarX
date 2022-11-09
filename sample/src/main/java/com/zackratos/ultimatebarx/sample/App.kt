4e051995170e7977d020c8177fbd3f046368c30a
墨宇 <zzh_hz@126.com>
2022-11-09 11:20:03 +0800

    gradle 版本

--- sample/src/main/java/com/zackratos/ultimatebarx/sample/App.kt
+++ sample/src/main/java/com/zackratos/ultimatebarx/sample/App.kt
@@ -1,8 +1,11 @@
 package com.zackratos.ultimatebarx.sample
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
+import android.util.Log
 
 /**
  * @Author   : zackratos
@@ -10,7 +13,7 @@ import android.content.Context
  * @email    : zhangwenchao@soulapp.cn
  * @Describe :
  */
-class App: Application() {
+class App : Application() {
 
     companion object {
         @SuppressLint("StaticFieldLeak")
@@ -20,6 +23,29 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         context = this
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                Log.e("----", "${activity.localClassName}")
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+            }
+
+            override fun onActivityPaused(activity: Activity) {
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+            }
+        })
     }
 
 }
